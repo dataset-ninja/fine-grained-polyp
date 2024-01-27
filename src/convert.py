effd24364c0124860fcb06ebd7971158a78cdeb2
peixed351 <peixed351@gmail.com>
2024-01-27 15:10:15 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,16 @@
-import supervisely as sly
+import csv
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +33,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +61,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +70,91 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    train_data_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_train/images"
+    val_data_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_valid/images"
+    test_data_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_test/images"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    train_masks_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_train/masks"
+    train_tags_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_train/train.csv"
 
-    # ... some code here ...
+    val_masks_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_valid/masks"
+    val_tags_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_valid/valid.csv"
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    test_masks_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_test/masks"
+    test_tags_path = "/home/alex/DATASETS/TODO/polyp_ash/polyp_ash/m_test/test.csv"
 
-    # return project
+    batch_size = 30
 
+    ds_name_to_split = {
+        "train": (train_data_path, train_masks_path, train_tags_path),
+        "val": (val_data_path, val_masks_path, val_tags_path),
+        "test": (test_data_path, test_masks_path, test_tags_path),
+    }
 
+    def create_ann(image_path):
+        labels = []
+
+        tags_data = image_name_to_tags[get_file_name(image_path)]
+        histologia = sly.Tag(histologia_meta, value=tags_data[0])
+        cls_tag = sly.Tag(cls_meta, value=tags_data[1])
+
+        mask_path = os.path.join(masks_path, get_file_name_with_ext(image_path))
+        mask_np = sly.imaging.image.read(mask_path)[:, :, 0]
+        img_height = mask_np.shape[0]
+        img_wight = mask_np.shape[1]
+        obj_mask = mask_np == 255
+        ret, curr_mask = connectedComponents(obj_mask.astype("uint8"), connectivity=8)
+        for i in range(1, ret):
+            obj_mask = curr_mask == i
+            curr_bitmap = sly.Bitmap(obj_mask)
+            curr_label = sly.Label(curr_bitmap, obj_class)
+            labels.append(curr_label)
+
+        return sly.Annotation(
+            img_size=(img_height, img_wight), labels=labels, img_tags=[histologia, cls_tag]
+        )
+
+    obj_class = sly.ObjClass("polyp", sly.Bitmap)
+
+    histologia_meta = sly.TagMeta("histologia", sly.TagValueType.ANY_STRING)
+    cls_meta = sly.TagMeta("cls", sly.TagValueType.ANY_STRING)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class], tag_metas=[histologia_meta, cls_meta])
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name, ds_data in ds_name_to_split.items():
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        images_path, masks_path, tags_path = ds_data
+
+        image_name_to_tags = {}
+        with open(tags_path, "r") as file:
+            csvreader = csv.reader(file)
+            for row in csvreader:
+                image_name_to_tags[row[0]] = row[1:]
+
+        images_names = os.listdir(images_path)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(images_path, image_name) for image_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,46 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Fine Grained Polyp"
+PROJECT_NAME_FULL: str = "Swin-Expand: Fine-Grained Polyp Segmentation and Classification"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical()]
+CATEGORY: Category = Category.Medical(sensitive_content=True)
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [
+    CVTask.InstanceSegmentation(),
+    CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
+]
+ANNOTATION_TYPES: List[AnnotationType] = [
+    AnnotationType.InstanceSegmentation(),
+    AnnotationType.ObjectDetection(),
+]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = "2023"
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://github.com/yaeltudela/polyp_ash"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 13169263
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/fine-grained-polyp"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://drive.google.com/file/d/1rmMLHohni3Vq_fZ-Ddt42vj6C6SSlkvW"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,19 +60,33 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://link.springer.com/chapter/10.1007/978-3-031-45249-9_4"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = [
+    "Yael Tudela",
+    "Jorge Berna",
+    "Ana García-Rodríguez",
+    "Glòria Fernández-Esparrach",
+]
+AUTHORS_CONTACTS: Optional[List[str]] = [
+    "ytudela@cvc.uab.cat",
+    "jbernal@cvc.uab.cat",
+    "anagrod4@gmail.com",
+    "mgfernan@clinic.cat",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "ES joint research group"
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "__POSTTEXT__": "Additionally, every image marked with its ***histologia*** and ***cls*** tag"
+}
 TAGS: Optional[List[str]] = None
 
 
